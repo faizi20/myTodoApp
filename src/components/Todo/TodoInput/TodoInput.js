ad814246a95d01fd5b9742a03f027cd3258db3b0
Faizan756 <faizan.khan@cubixlabs.com>
2022-10-25 18:40:10 +0500

    Change Css and package file

--- src/components/Todo/TodoInput/TodoInput.js
+++ src/components/Todo/TodoInput/TodoInput.js
@@ -1,21 +1,29 @@
 import React, { useState } from 'react'
 import Button from '../../UI/Button'
-import './TodoInput.css'
+import styles from './TodoInput.module.css'
 
 function TodoInput(props) {
-    const [enteredValue, setEnteredValue] = useState();
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true)
 
     const formSubmitHandler = (events) =>{
         events.preventDefault();
+        if(enteredValue.trim().length === 0){
+            setIsValid(false)
+            return
+        }
         props.onAddItem(enteredValue)
     }
 
     const goalInputChangeHandler = (events) =>{
+        if(events.target.value.trim().length > 0){
+            setIsValid(true)
+        } 
         setEnteredValue(events.target.value)
     }
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className='form-control'>
+            <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
                 <label>Todo App</label>
                 <input type='text' value={enteredValue} onChange={goalInputChangeHandler}/>
             </div>
